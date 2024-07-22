80b66853ba058af72144bbe0a8ef65be477bd7c8
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-22 13:06:04 +0900

    alt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
+extern crate nom;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+pub use nom::character::complete::space1;
+use nom::IResult;
 use std::error::Error;
 
-extern crate nom;
-// pub use nom::bytes::complete::tag;
-pub use nom::character::complete::alpha0;
-use nom::IResult;
-
-fn parser(input: &str) -> IResult<&str, &str> {
-    alpha0(input)
+fn parse_abc_or_def(input: &str) -> IResult<&str, &str> {
+    alt((tag("abc"), tag("def")))(input)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (remaining, letters) = parser("abc123")?;
-    assert_eq!(remaining, "123");
-    assert_eq!(letters, "abc");
+    let (leftover_input, output) = parse_abc_or_def("abcWorld")?;
+    assert_eq!(leftover_input, "World");
+    assert_eq!(output, "abc");
 
+    assert!(parse_abc_or_def("ghiWorld").is_err());
     Ok(())
 }
