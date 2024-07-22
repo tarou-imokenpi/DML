7ed4c1e84e177ce27f9710cf08290d39035d0204
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-22 23:28:49 +0900

    DMLParser (variable非対応)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,108 @@
-use std::error::Error;
+use pest::Parser;
+use pest_derive::Parser;
+use std::fs;
 
-use nom::bytes::complete::tag;
-use nom::character::complete::i32;
-use nom::sequence::{delimited, separated_pair};
-use nom::IResult;
-#[derive(Debug, PartialEq)]
-pub struct Coordinate {
-    pub x: i32,
-    pub y: i32,
+#[derive(Parser)]
+#[grammar = "dml.pest"]
+pub struct DMLParser;
+
+fn main() {
+    let input = fs::read_to_string("./test.dml").expect("Failed to read file");
+    let dml = DMLParser::parse(Rule::dml, &input)
+        .expect("Failed to parse")
+        .next()
+        .expect("No dml found");
+
+    process_dml(dml);
 }
 
-fn parse_integer_pair(input: &str) -> IResult<&str, (i32, i32)> {
-    separated_pair(i32, tag(", "), i32)(input)
+fn process_dml(dml: pest::iterators::Pair<Rule>) {
+    for record in dml.into_inner() {
+        for object in record.into_inner() {
+            match object.as_rule() {
+                Rule::variable => process_variable(object),
+                Rule::item => process_item(object),
+                Rule::group => process_group(object),
+                _ => println!("Unknown rule: {:?}", object.as_rule()),
+            }
+        }
+    }
 }
 
-fn parse_coordinate(input: &str) -> IResult<&str, Coordinate> {
-    let (remaining, (x, y)) = delimited(tag("("), parse_integer_pair, tag(")"))(input)?;
-
-    Ok((remaining, Coordinate { x, y }))
+fn process_variable(object: pest::iterators::Pair<Rule>) {
+    println!("Matched variable");
+    let mut inner_rules = object.into_inner();
+    let id = inner_rules.next().expect("No ID found").as_str();
+    let value = inner_rules.next().expect("No value found").as_str();
+    println!("Variable: {} = {}", id, value);
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let (_, parsed) = parse_coordinate("(3,5)")?;
+fn process_item(object: pest::iterators::Pair<Rule>) {
+    println!("Matched item");
+    let mut inner_rules = object.into_inner();
+    let item_name = inner_rules.next().expect("No item name found").as_str();
+    println!("Item: {}", item_name);
 
-    assert_eq!(parsed, Coordinate { x: 3, y: 5 });
+    for block in inner_rules {
+        parse_block(block);
+    }
+}
 
-    println!("{:?}", parsed);
+fn process_group(object: pest::iterators::Pair<Rule>) {
+    println!("Matched group");
+    let mut inner_rules = object.into_inner();
+    let group_name = inner_rules.next().expect("No group name found").as_str();
+    println!("Group: {}", group_name);
 
-    Ok(())
+    for ref_block in inner_rules {
+        parse_ref_block(ref_block);
+    }
+}
+
+fn parse_block(block: pest::iterators::Pair<Rule>) {
+    let block_name = block.as_str();
+    println!("  Block:{}", block_name);
+
+    for nested_block in block.into_inner() {
+        match nested_block.as_rule() {
+            Rule::field => {
+                let mut inner_fields = nested_block.into_inner();
+                let field_name = inner_fields.next().unwrap().as_str();
+                let field_value = inner_fields.next().unwrap().as_str();
+                println!("    Field: {} = {}", field_name, field_value);
+            }
+            Rule::block => parse_block(nested_block),
+            _ => {}
+        }
+    }
+}
+
+fn parse_ref_block(ref_block: pest::iterators::Pair<Rule>) {
+    let mut inner_rules = ref_block.into_inner();
+
+    while let Some(ref_item) = inner_rules.next() {
+        match ref_item.as_rule() {
+            Rule::reference => {
+                let mut ref_inner = ref_item.into_inner();
+                let item_name = ref_inner.next().unwrap().as_str();
+                let value = ref_inner.next().unwrap().as_str();
+                println!("  Ref: {} = {}", item_name, value);
+            }
+            Rule::nested_block => {
+                for nested in ref_item.into_inner() {
+                    match nested.as_rule() {
+                        Rule::reference => {
+                            let mut ref_inner = nested.into_inner();
+                            let item_name = ref_inner.next().unwrap().as_str();
+                            let value = ref_inner.next().unwrap().as_str();
+                            println!("  Ref: {} = {}", item_name, value);
+                        }
+                        Rule::block => parse_block(nested),
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
 }
